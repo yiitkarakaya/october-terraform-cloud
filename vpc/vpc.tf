b7a6e6924a9208e1f70752cad850649e364e4918
yiitkarakaya <92554257+yiitkarakaya@users.noreply.github.com>
2022-02-10 21:00:13 -0500

    added vpc

---
+++ vpc/vpc.tf
@@ -0,0 +1,17 @@
+variable "region" {
+    default = "us-east-1"
+}
+
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+  azs             = ["${var.region}", "${var.region}b", "${var.region}"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
